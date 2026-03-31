3bd30cff6cac6bfff07ef07f0725f8a0e7ee96b8
Tiphe0821 <namucho24@gmail.com>
2026-03-31 18:31:54 +0900

    [ feat ] 카드 클릭시 180도 회전 추가

--- Program_0317_1B_JHC/Assets/Scripts/Lecture03/Card.cs
+++ Program_0317_1B_JHC/Assets/Scripts/Lecture03/Card.cs
@@ -9,26 +9,46 @@ public class Card : MonoBehaviour
     // 변수
     public TextMeshProUGUI card;
     public int cardNum;
-
-    
-
-
+    public float rotateSpeed;
+    public bool isClick = false;
+    public Quaternion flipRotation = Quaternion.Euler(0, 180f, 0);
+    public Quaternion originRotation = Quaternion.Euler(0, 0, 0);
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         card = GetComponentInChildren<TextMeshProUGUI>();
 
+        cardNum = Random.Range(0, 10);
+
         card.text = cardNum.ToString();
-
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        //transform.Rotate(0, 180 * Time.deltaTime, 0);
+        // 0 => 180 => -180 => 0
+        //if (isClick)
+        //{
+        //    if (transform.eulerAngles.y >= 0 && transform.eulerAngles.y < 180)
+        //    {
+        //        transform.Rotate(0, rotateSpeed * Time.deltaTime, 0);
+        //    }
+        //}
+
+        if(isClick)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, flipRotation, rotateSpeed * Time.deltaTime);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, originRotation, rotateSpeed * Time.deltaTime);
+        }
     }
 
     // 카드 돌리기
+    public void ClickCard()
+    {
+        isClick = !isClick;
+    }
 }
